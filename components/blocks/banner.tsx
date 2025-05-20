cd5526994601d3653fbb7bd949eb6a6ec042579d
angyaguilr <aaguilar@elon.edu>
2025-05-20 12:22:21 -0400

    removed svg wave from banner

--- components/blocks/banner.tsx
+++ components/blocks/banner.tsx
@@ -11,13 +11,6 @@ import { Section } from '../layout/Section';
 export const Banner = ({ data }: { data: PageBlocksBanner }) => {
     return (
         <Section >
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" preserveAspectRatio="none">
-                <path
-                    fill="#0A121E"
-                    fillOpacity="1"
-                    d="M0,64L80,58.7C160,53,320,43,480,80C640,117,800,203,960,197.3C1120,192,1280,96,1360,48L1440,0L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z">    
-                </path>
-            </svg>
             <div className="text-center">
                 <h2 className="text-balance text-4xl font-semibold lg:text-5xl" data-tina-field={tinaField(data, 'title')}>{data.title}</h2>
                 <p className="mt-4" data-tina-field={tinaField(data, 'description')}>{data.description}</p>
@@ -70,6 +63,11 @@ export const bannerBlockSchema: Template = {
         },
     },
     fields: [
+        {
+          type: "image",
+          label: "BannerImg",
+          name: "bannerimg",
+        },
         {
             type: "string",
             label: "Title",
