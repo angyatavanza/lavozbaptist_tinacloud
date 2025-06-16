a08525d7d4628a226934b170a78553c62881a894
angyaguilr <aaguilar@elon.edu>
2025-06-16 16:04:43 -0400

    to-do

--- components/blocks/landing-about-us.tsx
+++ components/blocks/landing-about-us.tsx
@@ -21,7 +21,7 @@ import { cn } from "@/lib/utils";
 //done 7: add text to aboutus in homepage
 //done 8: decide if short aboutus or short mission should be on the homepage
 //to-do 21: extend the width of the div for the about-us image to be full-width
-//to-do 56: make sure all of the buttons have /links attached to them and check that the links work across the pages (when assigning a label to a button, this is the Error: Failed to assertShape - this must be a `object` type, but the final value was: `true`.)
+//to-do 59: make sure all of the buttons have /links attached to them and check that the links work across the pages (when assigning a label to a button, this is the Error: Failed to assertShape - this must be a `object` type, but the final value was: `true`.)
 
 const transitionVariants = {
   container: {
