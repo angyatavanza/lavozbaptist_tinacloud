3b1d44200a459f424f19b9ac738c88e249c593d7
angyaguilr <aaguilar@elon.edu>
2025-08-19 04:39:11 -0400

    static export

--- app/messages/[...urlSegments]/page.tsx
+++ app/messages/[...urlSegments]/page.tsx
@@ -2,8 +2,7 @@ import React from "react";
 import client from "@/tina/__generated__/client";
 import Layout from "@/components/layout/layout";
 import MessageClientPage from "./client-page";
-import { fetchFacebookVideos } from "../page";
-import { MessageQuery } from "@/tina/__generated__/types";
+import { notFound } from "next/navigation";
 
 export const dynamic = 'force-static';
 export const dynamicParams = false;
@@ -29,79 +28,9 @@ export default async function MessagePage({
       </Layout>
     );
   } catch (err) {
-    console.warn(
-      `MDX not found for slug ${filepath}, falling back to Facebook...`
-    );
+    return notFound();
   }
 
-  const facebookVideos = await fetchFacebookVideos();
-
-  const fbVideo = facebookVideos.find((video) => {
-    const dateSlug = video.date?.split("T")[0];
-    const safeSlug = video.title
-      .toLowerCase()
-      .replace(/[^a-z0-9]+/g, "-")
-      .replace(/(^-|-$)/g, "")
-      .slice(0, 50);
-    return `${dateSlug}-${safeSlug}` === filepath;
-  });
-
-  if (!fbVideo) return <div>Not Found</div>;
-
-  const fbData: MessageQuery = {
-    message: {
-      __typename: "Message",
-      id: fbVideo.id,
-      title: fbVideo.title,
-      date: fbVideo.date,
-      excerpt: fbVideo.excerpt,
-      _body: {
-        type: "root",
-        children: [],
-      },
-      image: {
-        __typename: "MessageImage",
-        videoUrl: fbVideo.videoUrl,
-        src: fbVideo.thumbnailUrl,
-        embeddable: fbVideo.embeddableUrl,
-      },
-      color: "blue",
-      coordinator: {
-        __typename: "Coordinator",
-        name: "La Voz De La Esperanza was live",
-        avatar: "/ve_logo.png",
-        id: `fb-${fbVideo.id}`,
-        _sys: {
-          __typename: "SystemInfo",
-          filename: `fb-${fbVideo.id}`,
-          basename: `fb-${fbVideo.id}`,
-          breadcrumbs: [filepath],
-          path: `fb-${fbVideo.id}`,
-          relativePath: `fb-${fbVideo.id}.mdx`,
-          extension: "mdx",
-        },
-      },
-      _sys: {
-        __typename: "SystemInfo",
-        filename: filepath,
-        basename: filepath,
-        path: filepath,
-        relativePath: filepath + ".mdx",
-        extension: "mdx",
-        breadcrumbs: [filepath],
-      },
-    },
-  };
-
-  return (
-    <Layout rawPageData={fbData}>
-      <MessageClientPage
-        data={fbData}
-        query=""
-        variables={{ relativePath: filepath + ".mdx" }}
-      />
-    </Layout>
-  );
 }
 
 export async function generateStaticParams() {
